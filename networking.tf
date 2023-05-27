c531e9991435ff7b2ed3a9533731a5eba1109f5d
Vitor Jr <vitor.junior@contaswap.com>
2023-05-27 09:26:01 -0300

    feat: ✨ add new variables (#8)
    Signed-off-by: Vitor Ugo Roda da Rosa Junior <vitor.junior@contaswap.com>

--- networking.tf
+++ networking.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
   tags = {
     "Name" = "${var.env_code}-epoch-vpc"
   }
 }
 
 resource "aws_subnet" "public" {
-  count             = length(local.public_cidr)
+  count             = length(var.public_cidr)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.public_cidr[count.index]
+  cidr_block        = var.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
   tags = {
     "Name" = "${var.env_code}-epoch-subnet-pub${count.index + 1}"
@@ -16,9 +16,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count             = length(local.private_cidr)
+  count             = length(var.private_cidr)
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.private_cidr[count.index]
+  cidr_block        = var.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
   tags = {
     "Name" = "${var.env_code}-epoch-subnet-priv${count.index + 1}"
@@ -45,13 +45,13 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(local.public_cidr)
+  count          = length(var.public_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
   vpc   = true
   tags = {
     Name = "${var.env_code}-epoch-eip-nat${count.index}"
@@ -59,7 +59,7 @@ resource "aws_eip" "nat" {
 }
 
 resource "aws_nat_gateway" "g" {
-  count         = length(local.public_cidr)
+  count         = length(var.public_cidr)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
   tags = {
@@ -71,7 +71,7 @@ resource "aws_nat_gateway" "g" {
 }
 
 resource "aws_route_table" "private" {
-  count  = length(local.private_cidr)
+  count  = length(var.private_cidr)
   vpc_id = aws_vpc.main.id
   route {
     cidr_block     = "0.0.0.0/0"
@@ -83,7 +83,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(local.private_cidr)
+  count          = length(var.private_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
 variable "env_code" {}
+variable "vpc_cidr" {}
+variable "public_cidr" {}
+variable "private_cidr" {}
