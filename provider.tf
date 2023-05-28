1615a4f703e7b40e4684b9901319f8a600544692
Vitor Jr <vitor@epoch-chrono.com>
2023-05-28 13:32:31 -0300

    feat: ✨ add remote s3 backend to provider block (#6)

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-mentoring-tf-remote-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
