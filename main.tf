ae0d985dfad615e2fe4efb22da69f05dc0e2f0b8
Vitor Jr <vitor@epoch-chrono.com>
2023-05-28 06:27:55 -0300

    feat: ✨ add gh approve/merge cli strategy to README.md (#17) (#1)

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c00453583aaf434e" # ubuntu 22.04 ARM for gravitron instances
-  instance_type = "t4g.micro"
-
-  tags = {
-    Name = "${var.env_code}-ExampleAppServerInstance"
-  }
-}
