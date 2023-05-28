65996df171733d896b74e17f0f8886f71c2f1258
Vitor Jr <vitor@epoch-chrono.com>
2023-05-28 11:31:32 -0300

    feat: ✨ add 2048 game to user data (#4)

--- user-data.sh
+++ user-data.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 yum update -y
-yum install -y httpd
+yum install -y httpd git
+git clone https://github.com/gabrielecirulli/2048.git
+cp -rp 2048/* /var/www/html
 systemctl enable --now httpd
