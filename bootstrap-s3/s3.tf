ed50c6b836e58995035d6850d165e868fa665623
Vitor Ugo Roda da Rosa Junior <vitor.junior@contaswap.com>
2023-05-28 13:21:07 -0300

    feat: ✨ add remote s3 backend to provider block
    Signed-off-by: Vitor Ugo Roda da Rosa Junior <vitor.junior@contaswap.com>

--- bootstrap-s3/s3.tf
+++ bootstrap-s3/s3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "tf-remote-state" {
   bucket = "terraform-mentoring-tf-remote-state"
 
   tags = {
-    Name        = "${var.env_code}-epoch-tf-remote-state"
+    Name = "${var.env_code}-epoch-tf-remote-state"
   }
 }
